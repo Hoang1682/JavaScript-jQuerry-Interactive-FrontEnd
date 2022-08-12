7ff30d0fdfc7418f53a870c1bf31ea7eb8b24c68
Hoang1682 <daibangnon9x@gmail.com>
2022-08-12 18:41:40 +0700

    loading json with ajax

--- Ajax and Json.js
+++ Ajax and Json.js
@@ -56,3 +56,33 @@ xhr.onload = function() {
 // prepare the request and send the request
 xhr.open('GET', 'data/data.xml', true);
 xhr.send(null);
+
+
+
+// LOADING JSON WIHT AJAX 
+var xhr = new XMLHttpRequest(); // create xmlhttprequest object
+
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        responseObject = JSON.parse(xhr.responseText);
+
+        // build up string with new content (could also use DOM manipulation)
+        var newContent = '';
+        for (var i = 0; i < responseObject.events.length; i++) {
+            newContent += '<div class="event">';
+            newContent += '<img src="' + responseObject.events[i].map + '" ';
+            newContent += 'alt=" ' +responseObject.events[i].location + ' " />';
+            newContent += '<p><b>' + responseObject.events[i].location + '</b><br>';
+            newContent += responseObject.events[i].date + '</p>';
+            newContent += '</div>';
+        }
+
+        // update the page with new content
+        document.getElementById('content').innerHTML = newContent;
+
+    }
+};
+
+// prepare and send request
+xhr.open('GET', 'data/data.json', true);
+xhr.send(null);
